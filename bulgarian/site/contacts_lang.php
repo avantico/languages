8f9c19d8914e2045fdb5965eb35f5dbaa5b70b5c
Anton Ozolin <anton.ozolin@gmail.com>
2012-12-12 23:54:45 +0400

    внёс правки перед отъездом

--- bulgarian/site/contacts_lang.php
+++ bulgarian/site/contacts_lang.php
@@ -1,5 +1,29 @@
 <?
 
-$lang['site_contacts_aaa'] = 'aaa';
-$lang['site_contacts_bbb'] = 'bbb';
-$lang['site_contacts_ccc'] = 'ccc';
+$lang['site_contacts_page_title'] = 'Контакти';
+
+$lang['site_contacts_tel_block'] = 'Телефони';
+
+$lang['site_contacts_tel_bg'] = 'България';
+$lang['site_contacts_tel_ru'] = 'Русия';
+
+$lang['site_contacts_post_block'] = 'Пощенски адрес';
+$lang['site_contacts_post_look_map'] = 'Виж картата';
+
+$lang['site_contacts_contact_form_into'] = 'Можете също така да зададете въпрос в тази форма:';
+
+$lang['site_contacts_contact_subject_select'] = 'Изберете тема';
+$lang['site_contacts_contact_subject_main'] = 'Общ въпрос';
+$lang['site_contacts_contact_subject_money'] = 'Финансов проблем';
+$lang['site_contacts_contact_subject_techno'] = 'Технически въпрос';
+
+$lang['site_contacts_contact_name_title'] = 'Вашето име';
+$lang['site_contacts_contact_name_must_fill'] = 'Вие не уточни името на';
+
+$lang['site_contacts_contact_email_title'] = 'Вашият е-мейл';
+$lang['site_contacts_contact_email_must_fill'] = 'Вие не въведете имейл';
+
+$lang['site_contacts_contact_tel_title'] = 'Вашият телефон';
+$lang['site_contacts_contact_message_title'] = 'Текстовите съобщения';
+
+$lang['site_contacts_contact_message_send'] = 'Изпращам';
