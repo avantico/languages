e60840abecb6da930723c17c049e1dba25fbff1a
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 15:45:58 +0400

    Revert "how_it_works ready 100%"
    This reverts commit cd800d935c2de09320213653dca820b009c498ba.

--- english/site/how_it_works_lang.php
+++ english/site/how_it_works_lang.php
@@ -1,29 +1,29 @@
 <?
 /* Файл с переводами страницы Как это работает  */
 
-$lang['site_how_page_title'] = 'How it works?';
-$lang['site_how_page_title_description'] = 'It’s simple and efficient: ';
+$lang['site_how_page_title'] = 'Как это работает?';
+$lang['site_how_page_title_description'] = 'Если коротко, то все очень просто! Вот так:';
 
 
-$lang['site_how_action_step_1_scheme'] = 'Business registers at Glex, specifies reward for an Agent and bonus for a Tourist.';
-$lang['site_how_action_step_1_description'] = 'Business representative registers at Glex.com and specifies agent’s reward for the referred customer as well as special offer or a bonus that will be available to the referred customer.';
+$lang['site_how_action_step_1_scheme'] = 'Заведение регистрируется в Glex, указывает вознаграждение для Агента и бонус для Туриста.';
+$lang['site_how_action_step_1_description'] = 'Заведение регистрируется в Glex.com и указывает в системе агентское вознаграждение, которое оно готово платить агенту за приведенного туриста. А также — бонус или скидку, которую получит турист, пришедший по рекомендации агента.';
 
-$lang['site_how_action_step_2_scheme'] = 'Agent registers with Glex acquiring unique QR-code and promotional materials';
-$lang['site_how_action_step_2_description'] = 'After registering with Glex Agent acquires unique identifiers: QR-code and Glex wallet ID. These identifiers can be printed on a map with businesses providing special offers, on a flyer with a list of businesses, on a travel card or published on a web-site. Once registered with Glex,  Agent also gets access to the tools that allow him to create customised maps, flyers and widgets for website.';
+$lang['site_how_action_step_2_scheme'] = 'Агент регистрируется в Glex, получает уникальный QR-код и рекламные материалы';
+$lang['site_how_action_step_2_description'] = 'Регистрируясь в Glex.com, Агент получает уникальные идентификаторы — QR-код и номер кошелька в системе Glex. Полученные идентификаторы можно распечатать на карте с Заведениями, дающими скидки, листовке со списком Заведений, карточке туриста или разместить на веб-сайте. Возможность быстро создавать готовые карты, листовки, виджеты для веб-сайта появляется у Агента после регистрации в Glex.';
 
-$lang['site_how_action_step_3_scheme'] = 'Agent gives to a Tourist promotional materials from the Business (map, brochure etc) contating his QR-code.';
-$lang['site_how_action_step_3_description'] = 'Once flyers, maps or other advertising media containing information about recommended Businesses and QR-code are ready, Agent offers them to tourists and tells what discounts or bonuses he/she is eligible for as a bearer of QR-code.';
+$lang['site_how_action_step_3_scheme'] = 'Агент передает Туристу рекламу Заведения (карта, буклет и так далее) со своим QR-кодом.';
+$lang['site_how_action_step_3_description'] = 'После подготовки листовок, карт или иных рекламных носителей с рекомендуемыми Заведениями и своим QR-кодом Агент предлагает их туристу и рассказывает ему о скидке или бонусе, который он получит в Заведении как предъявитель QR-кода.';
 
-$lang['site_how_action_step_4_scheme'] = 'Tourist shows QR-code and receives a bonus. Business scans QR-code and records Agent’s reward into Glex system.';
-$lang['site_how_action_step_4_description'] = 'Tourist goes to the recommended Business, shows advertising media  acquired from the Agent and receives a discount or a bonus. Business representative scans QR-code with free mobile application Glex and records Agent’s reward for the referred Tourist. Alternatively, rewards can be recorded using Glex web-site.';
+$lang['site_how_action_step_4_scheme'] = 'Турист предъявляет QR-код и получает бонус. Заведение сканирует QR-код и записывает в Glex вознаграждение Агенту.';
+$lang['site_how_action_step_4_description'] = 'Турист обращается в рекомендованное Заведение, предъявляет полученный от Агента рекламный носитель и получает скидку или бонус. Сотрудник Заведения сканирует QR-код с помощью бесплатного мобильного приложения Glex для смартфона и записывает в систему размер вознаграждения Агенту за приведенного Туриста. Также записать вознаграждение можно с помощью веб-сайта Glex.com.';
 
-$lang['site_how_action_step_5_scheme'] = 'Business regularly transfers accumulated rewards to Glex.';
-$lang['site_how_action_step_5_description'] = 'Business remunerates all the Agents for referred customers monthly through single transaction payable to Ozolin Srl.';
+$lang['site_how_action_step_5_scheme'] = 'Заведение регулярно переводит накопившиеся агентские вознаграждения в Glex.';
+$lang['site_how_action_step_5_description'] = 'Заведение один раз в месяц переводит одним платежом накопившиеся вознаграждения всем Агентам, порекомендовавшим Заведение, в компанию Glex GmbH.';
 
-$lang['site_how_action_step_6_scheme'] = 'Glex pays the rewards to all the Agents that have been recommending the Business.';
-$lang['site_how_action_step_6_description'] = 'Ozolin Srl transfers received rewards to the Agents, deducting handling and service commission.';
+$lang['site_how_action_step_6_scheme'] = 'Glex выплачивает вознаграждения всем Агентам, рекомендовавшим Заведения.';
+$lang['site_how_action_step_6_description'] = 'Glex GmbH выплачивает полученные вознаграждения Агентам, оставляя себе процент за обеспечение расчетов и сервиса.';
 
 
-$lang['site_how_after_sheme_intro_more'] = 'If you would like to know more, please, refer to the specialised sections of the site:';
+$lang['site_how_after_sheme_intro_more'] = 'Если вы хотите узнать подробнее о работе с системой, воспользуйтесь специализированными разделами сайта:';
 
-$lang['site_how_after_sheme_intro_register'] = 'If you like this idea, register now and start earning more with Glex!';
+$lang['site_how_after_sheme_intro_register'] = 'Если все понятно, то регистрируйтесь сейчас и начните зарабатывать с Glex';
