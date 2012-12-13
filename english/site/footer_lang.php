7f1e994ae26f49139794a1ff4947e9ef8cdfef41
Ksenia Marinina <ksenia.marinina@glex.com>
2012-12-13 16:18:25 +0400

    footer

--- english/site/footer_lang.php
+++ english/site/footer_lang.php
@@ -1,8 +1,16 @@
 <?
 
-$lang['site_footer_aaa']    = "aaa";
-$lang['site_footer_bbb'] 	= "bbb";
-$lang['site_footer_ccc']	= "ccc";
+$lang['site_footer_top_link']    = "Вверх";
+$lang['site_footer_rights_reserved'] 	= "© 2012 Glex. All rights reserved.";
+
+$lang['site_footer_subscribe_intro']	= "Читайте главные новости Glex, рассказы о нашей жизни и о новых способах заработка в удобной вам форме";
+$lang['site_footer_subscribe_outro']	= "рассылка не чаще одного раза в месяц";
+$lang['site_footer_subscribe_btn']	= "Подписаться";
+
+$lang['site_footer_menu_terms']	= "Условия использования";
+
+$lang['site_footer_address']	= "Glex GmbH, Merckstrasse 9, 91522 Ansbach";
+
 
 
 
