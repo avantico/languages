47c839a104b9b8b517b24b8d876f7e140c77d20f
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 15:46:09 +0400

    Revert "Revert "about_lang ready 100%""
    This reverts commit c3098cd140312f10a6a60178cd690d975139786c.

--- english/site/about_lang.php
+++ english/site/about_lang.php
@@ -1,19 +1,19 @@
 <?
 
-$lang['site_about_page_desc'] = 'Glex GmbH — немецкая компания, головной офис которой находится в одном из деловых центров Баварии — городе Нюрнберг.';
+$lang['site_about_page_desc'] = 'Ozolin Srl is an Italian company.';
 
 
 
-$lang['site_about_glex_unique_title'] = 'Glex - уникальный сервис';
+$lang['site_about_glex_unique_title'] = 'Glex is a unique service';
 $lang['site_about_glex_unique_desc'] = 'Glex has combined minds of professonals from international tourism industy and experienced speclalists of builidng web services and internet business. Together we develop this project with the unique idea we love so much.';
 
-$lang['site_about_glex_geo_title'] = 'География Glex';
+$lang['site_about_glex_geo_title'] = 'Geograhy of Glex';
 $lang['site_about_glex_geo_desc'] = 'For the time being, we already work in Europe but within the next one to two years we are going to expand to other major tourist industries all over the world.';
 
-$lang['site_about_glex_mission_title'] = 'Наша миссия';
+$lang['site_about_glex_mission_title'] = 'Our mission';
 $lang['site_about_glex_mission_desc'] = 'Our mission is to build an effective and easy to use system that will provide additional services for tourists. give new clients to tourist attractions and allow agnets earn more on useful recommendations to tourists.';
 
 $lang['site_about_links_how_it_works'] = 'You can find out more about this system in section';
-$lang['site_about_links_blog'] = 'Неформальный рассказ о жизни и развитии Glex читайте в блоге проекта';
-$lang['site_about_links_subscribe'] = 'Становитесь нашим подписчиком <br/>в ';
-$lang['site_about_links_subscribe_or'] = 'или';
+$lang['site_about_links_blog'] = 'You can also read more about the development process of Glex in a blog';
+$lang['site_about_links_subscribe'] = 'Like us on Facebook!  <br/> ';
+$lang['site_about_links_subscribe_or'] = 'Follow us on Twitter!';
