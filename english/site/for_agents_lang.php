8941b863cf748bae56b1677ee210f0ce707e606b
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 15:46:09 +0400

    Revert "Revert "for_agents_lang ready 100%""
    This reverts commit 93693de82915b6ebb017747200ae2cf75a3e637c.

--- english/site/for_agents_lang.php
+++ english/site/for_agents_lang.php
@@ -1,2 +1,105 @@
 <?
 
+$lang['site_agents_intro_1'] = 'Do you often  <strong>deal with tourists</strong> and recommend various establishments such as restaurants, shops, rentals?';
+$lang['site_agents_intro_2'] = 'Using Glex you can do it more effective, more convenient, faster and <strong>earn more!</strong>';
+
+$lang['site_agents_how_to_earn_money_title'] = 'How to earn with Glex?';
+$lang['site_agents_how_to_earn_money_desc'] = 'Полностью о том, как работает наша система вы можете узнать на схеме';
+
+$lang['site_agents_how_to_earn_money_step_1'] = 'Sign up on the website';
+$lang['site_agents_how_to_earn_money_step_2'] = 'Get your perosonal agent QR-code';
+$lang['site_agents_how_to_earn_money_step_3'] = 'Create advertising media containing your QR-code';
+$lang['site_agents_how_to_earn_money_step_4'] = 'Give the media to your tourists';
+$lang['site_agents_how_to_earn_money_step_5'] = 'Get ready to recieve your reward for the referred tourists';
+
+$lang['site_agents_who_can_earn_money_title'] = 'Who can earn with Glex?';
+
+$lang['site_agents_table_colunn_agent'] = 'Agent type';
+$lang['site_agents_table_colunn_how'] = 'How to use Glex?';
+$lang['site_agents_table_colunn_type'] = 'Advertising media';
+
+$lang['site_agents_table_row_hotel_agent'] = 'Hotel';
+$lang['site_agents_table_row_hotel_how_li_1'] = 'Distribute prepared flyers with recommendations at the front desk';
+$lang['site_agents_table_row_hotel_how_li_2'] = 'Instantly create customised recommendations based on your clients requests';
+$lang['site_agents_table_row_hotel_how_li_3'] = 'Place the advertising media in the rooms';
+$lang['site_agents_table_row_hotel_type_li_1'] = 'Prepared lists of the recommended businesses using Glex';
+$lang['site_agents_table_row_hotel_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+
+$lang['site_agents_table_row_incoming_touroperator_agent'] = 'Tour operator receiving tourists ';
+$lang['site_agents_table_row_incoming_touroperator_how_li_1'] = 'Hand out flyers and special travel cards to your tourists in tour-buses, hotels and other points of contact';
+$lang['site_agents_table_row_incoming_touroperator_how_li_2'] = 'Using Glex, create the system that accounts recommendations that are given by your employees as well as received rewards';
+$lang['site_agents_table_row_incoming_touroperator_type_li_1'] = 'Prepared lists of the recommended businesses located in your city using Glex';
+$lang['site_agents_table_row_incoming_touroperator_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+$lang['site_agents_table_row_outgoing_touroperator_agent'] = 'Tour operator sending tourists';
+$lang['site_agents_table_row_outgoing_touroperator_how_li_1'] = 'Distribute flyers with recommended businesses based on the chosen destination';
+$lang['site_agents_table_row_outgoing_touroperator_type_li_1'] = 'Prepared lists of the recommended businesses located in your city using Glex';
+$lang['site_agents_table_row_outgoing_touroperator_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+$lang['site_agents_table_row_travel_agency_agent'] = 'Travelling agency';
+$lang['site_agents_table_row_travel_agency_how_li_1'] = 'Distribute flyers with recommended businesses based on the chosen destination';
+$lang['site_agents_table_row_travel_agency_type_li_1'] = 'Prepared lists of the recommended businesses located in your city using Glex';
+$lang['site_agents_table_row_travel_agency_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+$lang['site_agents_table_row_travel_web_site_agent'] = 'Web-site targeted at tourists';
+$lang['site_agents_table_row_travel_web_site_how_li_1'] = 'Publish customised widgets that will allow to provide automatic recommendations based on a chosen city';
+$lang['site_agents_table_row_travel_web_site_type_li_1'] = 'Customised widgets that allow user to create and print selected offers for the businesses at chosen destination';
+
+
+$lang['site_agents_table_row_travel_mobile_app_agent'] = 'Mobile application for tourists';
+$lang['site_agents_table_row_travel_mobile_app_how_li_1'] = 'Show information about special offer for the users and your Glex QR-code on the pages about the businesses that will attract tourists';
+$lang['site_agents_table_row_travel_mobile_app_how_li_2'] = 'Within your application, publish the lists of recommended businesses on the pages about cities and countries';
+$lang['site_agents_table_row_travel_mobile_app_type_li_1'] = 'Glex QR-code on the pages related to the businesses within your application';
+$lang['site_agents_table_row_travel_mobile_app_type_li_2'] = 'Lists of the recommended businesses published on the pages about cities and countries ';
+
+$lang['site_agents_table_row_excursions_agent'] = 'Sightseeing company';
+$lang['site_agents_table_row_excursions_how_li_1'] = 'Offer your tourists brochures and special travel cards during or prior a tour.';
+$lang['site_agents_table_row_excursions_type_li_1'] = 'Prepared lists of the recommended businesses located in your city using Glex';
+$lang['site_agents_table_row_excursions_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+$lang['site_agents_table_row_taxi_agent'] = 'Taxi services';
+$lang['site_agents_table_row_taxi_how_li_1'] = 'Destribute flyers and travel cards to tourists you pick up in your a taxi';
+$lang['site_agents_table_row_taxi_type_li_1'] = 'Prepared lists of recommended businesses located in your city using Glex';
+$lang['site_agents_table_row_taxi_type_li_2'] = 'Travel cards containing QR-code offering discounts in any businesses with Glex logo';
+
+
+$lang['site_agents_table_row_press_agent'] = 'Printed media';
+$lang['site_agents_table_row_press_how_li_1'] = 'Publish recommended businesses offering discounts in your magazine ';
+$lang['site_agents_table_row_press_type_li_1'] = 'The list of businesses in a printed edition';
+$lang['site_agents_table_row_press_type_li_2'] = '“Special offers” section etc';
+
+$lang['site_agents_table_row_advertisement_agent'] = 'Advertising agency';
+$lang['site_agents_table_row_advertisement_how_li_1'] = 'Offer tourists selected recommendations for businesses offering bonuses and located in your city';
+$lang['site_agents_table_row_advertisement_type_li_2'] = 'Flyers and brochures';
+
+
+$lang['site_agents_why_glex_title'] = 'Why should I work with Glex?';
+
+$lang['site_agents_why_glex_money_title'] = 'Greater rewards!';
+$lang['site_agents_why_glex_money_desc'] = 'You receive an opportunity to effectively earn from recommendations you make. And if you already provide recommendations, than Glex will make this process effortless and far more profitable.';
+
+$lang['site_agents_why_glex_auto_title'] = 'It’s automated!';
+$lang['site_agents_why_glex_auto_desc'] = 'Glex automatically accounts for all the rewards you receive as an agent. The reports about the rewards including detailed information about businesses is available in your profile.';
+
+$lang['site_agents_why_glex_time_title'] = 'Save time!';
+$lang['site_agents_why_glex_time_desc'] = 'You do not need to have a personal agreement with a business and keep track of all the transactions. All agreements and transfers are automated and performed by Glex.';
+
+$lang['site_agents_why_glex_base_title'] = 'Business network!';
+$lang['site_agents_why_glex_base_desc'] = 'You have access to international up-to-date network of businesses that are ready to reward agents for referred customers and provide these customers with special offers and bonuses.';
+
+$lang['site_agents_why_glex_client_title'] = 'Customer loyalty!';
+$lang['site_agents_why_glex_client_desc'] = 'You are offering your tourist an additional service by providing useful information about discounts and bonuses.';
+
+$lang['site_agents_why_glex_border_title'] = 'Glex has no limits!';
+$lang['site_agents_why_glex_border_desc'] = 'QR-code is not connected to the recommendations made by an Agent. Once Agent provides advertising media with his QR-code - it can be used by tourist in various  cities and countries.';
+
+$lang['site_agents_get_adv_materials_title'] = 'Receive ready-to-use advertising material with your QR-code!';
+$lang['site_agents_get_adv_materials_desc'] = 'Once you’re registered with Glex, you receive ready-to-use advertising media in several formats. This advertising media will automatically be embedded with your QR-code and your logo (optional). In just few clicks you can create:';
+
+$lang['site_agents_get_adv_material_list'] = 'Ready to print lists of businesses attracting tourists located in your city in an A4 format';
+$lang['site_agents_get_adv_material_widget'] = 'Widget for your web-site containing information about businesses in a specific city or country';
+$lang['site_agents_get_adv_material_card'] = 'Business card sized travel card with your QR code and your logo(optional) ';
+
+$lang['site_agents_outro'] = 'We’ve created Glex to make your business more comfortable and more profitable!';
+$lang['site_agents_outro_register'] = 'The only thing left for you is to register with Glex and start using it!';
