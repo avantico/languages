484d2b7a84b8e77d7699198c07152a4cc220e127
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 17:46:04 +0400

    Fixes

--- russian/site/registration_login_lang.php
+++ russian/site/registration_login_lang.php
@@ -1,28 +1,43 @@
 <?
 
-$lang['site_login_header'] = 'Введите учетные данные';
-$lang['site_login_login_title'] = 'E-mail';
-$lang['site_login_password_title'] = 'Пароль';
-$lang['site_login_login_action'] = 'Войти';
-$lang['site_login_forgot_password_link'] = 'Забыли пароль?';
+$lang['site_login_title'] 						= 'Вход в систему';
+$lang['site_login_header'] 						= 'Введите учетные данные';
+$lang['site_login_login_title'] 				= 'E-mail';
+$lang['site_login_password_title'] 				= 'Пароль';
+$lang['site_login_login_action'] 				= 'Войти';
 
-$lang['site_forgot_password_header'] = 'Восстановление пароля';
-$lang['site_forgot_password_enter_email'] = 'Введите E-mail на который производилась регистрация';
-$lang['site_forgot_password_action'] = 'Восстановить';
+$lang['site_login_remind_link'] 				= 'Забыли пароль?';
+$lang['site_login_remind_title'] 				= 'Восстановление пароля';
+$lang['site_login_remind_header'] 				= 'Введите E-mail на который производилась регистрация';
+$lang['site_login_remind_login_title'] 			= 'E-mail';
+$lang['site_login_remind_action'] 				= 'Восстановить';
+$lang['site_login_remind']						= "Забыли пароль?";
 
+$lang['site_login_remind_confirm_title']		= "Восстановление пароля";
+$lang['site_login_remind_confirm_text']			= "Для завершения процедуры восстановления пароля, введите новый пароль";
+$lang['site_login_remind_confirm_submit']		= "Восстановить пароль";
+$lang['site_login_remind_confirm_placeholder']	= "Введите пароль";
+
+$lang['site_login_remind_success_title']		= "Восстановление пароля";
+$lang['site_login_remind_success_text']			= "Все прошло хорошо! Пароль изменен, и вы можете войти";
+
+$lang['site_login_remind_success_email_title']	= "Восстановление пароля";
+$lang['site_login_remind_success_email_text']	= "Мы отправили вам ссылку для восстановления пароля на указанный E-mail";
+
+$lang['site_login_remind_error_title']			= "Восстановление пароля";
+$lang['site_login_remind_error_text']			= "Произошла ошибка при восстановлении пароля! Попробуйте заново";
+
+$lang['site_login_remind_email_header']			= "Восстановление пароля";
+$lang['site_login_remind_email_text']			= "Для изменения пароля перейдите по ссылке:";
 
 
 $lang['site_register_title'] 					= 'Регистрация';
-
 $lang['site_register_user_email'] 				= 'E-mail';
 $lang['site_register_user_email_placeholder'] 	= 'Введите адрес электронной почты';
-
 $lang['site_register_user_password'] 			= 'Пароль';
-$lang['site_register_user_password_placeholder']	= 'Введите пароль';
-
+$lang['site_register_user_password_placeholder']= 'Введите пароль';
 $lang['site_register_user_tmp_name'] 			= 'Компания';
-$lang['site_register_user_tmp_name_placeholder'] = 'Ваше имя или название компании';
-
+$lang['site_register_user_tmp_name_placeholder']= 'Ваше имя или название компании';
 $lang['site_register_jurisdiction_check']		= 'Выберите...';
 $lang['site_register_jurisdiction_id'] 			= 'Юрисдикция';
 
@@ -37,11 +52,11 @@ $lang['site_register_submit'] 					= 'Зарегистрироваться';
 
 $lang['site_register_errors_required']			= 'Это поле необходимо заполнить.';
 $lang['site_register_errors_valid_email']		= 'Вы ввели неправильный e-mail.';
-$lang['site_register_errors_min_length']			= 'Поле "%s" должно быть длиной минимум %s символов.';
-$lang['site_register_errors_max_length']			= 'Поле "%s" должно быть длиной максимум %s символов.';
-$lang['site_register_errors_is_unique'] 			= 'Такой %s уже существует!';
+$lang['site_register_errors_min_length']		= 'Поле "%s" должно быть длиной минимум %s символов.';
+$lang['site_register_errors_max_length']		= 'Поле "%s" должно быть длиной максимум %s символов.';
+$lang['site_register_errors_is_unique'] 		= 'Такой %s уже существует!';
 $lang['site_register_errors_main'] 				= 'Возникли ошибки при заполнении формы!';
-$lang['site_register_errors_agreement'] 			= 'Подтвердите согласие с условиями';
+$lang['site_register_errors_agreement'] 		= 'Подтвердите согласие с условиями';
 
 $lang['site_confirm_email_header']				= 'Подтверждение регистрации на Glex.com';
-$lang['site_confirm_email_text']					= 'Для подтверждения регистрации перейдите, пожалуйста по ссылке: ';
+$lang['site_confirm_email_text']				= 'Для подтверждения регистрации перейдите, пожалуйста по ссылке: ';
