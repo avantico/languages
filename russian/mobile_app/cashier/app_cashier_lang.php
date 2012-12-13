a91f5cc2b4a53a106b3c23ab7a1b8677d37a2e11
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 13:55:55 +0400

    Изменения

--- russian/mobile_app/cashier/app_cashier_lang.php
+++ russian/mobile_app/cashier/app_cashier_lang.php
@@ -1,9 +1,5 @@
 <?php
 
-$lang['app_cashier_error_title'] = "Ошибка";
-$lang['app_cashier_loading_title'] = "Загрузка";
-
-
 $lang['app_cashier_login_btn'] = "Войти";
 
 
@@ -12,24 +8,8 @@ $lang['app_cashier_login_title'] = "Авторизация";
 $lang['app_cashier_login'] = "E-mail или ID кассира";
 $lang['app_cashier_password'] = "Пароль";
 
-$lang['app_cashier_error_title'] = "Ошибка";
+$lang['app_cashier_login_btn'] = "Войти";
 
-$lang['app_cashier_login_error_auth'] = "Вы ввели неправильные данные авторизации";
-
-$lang['app_cashier_new_credit_title'] = "Новое начисление";
-$lang['app_cashier_enter_qr_title'] = "Введите номер QR-кошелька";
-
-$lang['app_cashier_enter_qr_placeholder'] = "Номер QR-кошелька";
-
-
-$lang['app_cashier_enter_qr_next'] = "Далее";
-
-$lang['app_cashier_enter_sum_title'] = "Введите сумму";
-
-$lang['app_cashier_enter_sum_placeholder'] = "Сумма начисления";
-$lang['app_cashier_enter_coment_placeholder'] = "Комментарий (необязательно)";
-
-$lang['app_cashier_checkout_new_credit'] = "Сохранить";
 
 
 /* End of file app_cashier_lang.php */
