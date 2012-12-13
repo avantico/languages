6693e2e214fe169f48e8c1bc11960a4a5f36691d
Ksenia Marinina <ksenia.marinina@glex.com>
2012-12-13 16:36:49 +0400

    Revert "contacts"
    This reverts commit a5f4cff6b302da9675a07220d7e94ff061d494a1.

--- english/site/contacts_lang.php
+++ english/site/contacts_lang.php
@@ -1,27 +1,5 @@
 <?
 
-$lang['site_contacts_page_title'] = 'Контакты';
-
-$lang['site_contacts_tel_block'] = 'Телефоны';
-
-$lang['site_contacts_tel_bg'] = 'Болгария';
-$lang['site_contacts_tel_ru'] = 'Россия';
-
-$lang['site_contacts_post_block'] = 'Почтовый адрес';
-$lang['site_contacts_post_look_map'] = 'Смотреть на карте';
-
-$lang['site_contacts_contact_form_into'] = 'Также вы можете задать вопрос в этой форме:';
-
-$lang['site_contacts_contact_subject_select'] = 'Выберите тему';
-$lang['site_contacts_contact_subject_main'] = 'Общий вопрос';
-$lang['site_contacts_contact_subject_money'] = 'Финансовый вопрос';
-$lang['site_contacts_contact_subject_techno'] = 'Технический вопрос';
-
-$lang['site_contacts_contact_name_title'] = 'Ваше имя';
-$lang['site_contacts_contact_name_must_fill'] = 'Вы не указали имя';
-
-$lang['site_contacts_contact_email_title'] = 'Ваш e-mail';
-$lang['site_contacts_contact_email_must_fill'] = 'Вы не указали e-mail';
-
-$lang['site_contacts_contact_tel_title'] = 'Ваш телефон';
-$lang['site_contacts_contact_message_title'] = 'Текст сообщения';
+$lang['site_contacts_aaa'] = 'aaa';
+$lang['site_contacts_bbb'] = 'bbb';
+$lang['site_contacts_ccc'] = 'ccc';
