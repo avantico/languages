4642b8d579fb18fd9280a7cee282a2d11845a71a
Георгий Вяземский <msnake@avantico.ru>
2012-12-13 15:45:58 +0400

    Revert "Изменения"
    This reverts commit a91f5cc2b4a53a106b3c23ab7a1b8677d37a2e11.

--- bulgarian/site/header_lang.php
+++ bulgarian/site/header_lang.php
@@ -5,7 +5,7 @@
 /* меню */
 $lang['site_header_menu_how_it_works']				= "Как работи това";
 $lang['site_header_menu_for_agents']			= "Агенти";
-$lang['site_header_menu_for_merchants']		= "Заведения";
+$lang['site_header_menu_for_merchants']		= "Места (Заведения)";
 $lang['site_header_menu_faq']		= "Въпроси и отговори";
 $lang['site_header_menu_about']		= "За Нас";
 $lang['site_header_menu_contacts']		= "За контакти";
