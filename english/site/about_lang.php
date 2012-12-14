108ef215e3129d64f4bc1722f93e9b6f4372cac3
Ksenia Marinina <ksenia.marinina@glex.com>
2012-12-14 17:32:47 +0400

    исправила еще в about

--- english/site/about_lang.php
+++ english/site/about_lang.php
@@ -11,7 +11,7 @@ $lang['site_about_glex_geo_title'] = 'Geograhy of Glex';
 $lang['site_about_glex_geo_desc'] = 'For the time being, we already work in Europe but within the next one to two years we are going to expand to other major tourist industries all over the world.';
 
 $lang['site_about_glex_mission_title'] = 'Our mission';
-$lang['site_about_glex_mission_desc'] = 'Our mission is to build an effective and easy to use system that will provide additional services for tourists. give new clients to tourist attractions and allow agnets earn more on useful recommendations to tourists.';
+$lang['site_about_glex_mission_desc'] = 'Our mission is to build an effective and easy to use system that will provide additional services for tourists. Give new clients to tourist attractions and allow agnets earn more on useful recommendations to tourists.';
 
 $lang['site_about_links_how_it_works'] = 'You can find out more about this system in section';
 $lang['site_about_links_blog'] = 'You can also read more about the development process of Glex in a blog';
