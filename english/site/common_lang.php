dee599b2cee106f7c6920e3e2c7042a2f7238688
Ksenia Marinina <ksenia.marinina@glex.com>
2012-12-14 15:34:06 +0400

    common_lang 100% ready

--- english/site/common_lang.php
+++ english/site/common_lang.php
@@ -1,31 +1,31 @@
 <?
 /* Фразы, встречающиеся на многих страницах сайта (Например, Агент, Турист, Заведение.)  */
 /* Вход, регистрация */
-$lang['site_common_login_btn'] = "Вход";
-$lang['site_common_registration_btn'] = "Регистрация";
+$lang['site_common_login_btn'] = "Sign in";
+$lang['site_common_registration_btn'] = "Registration";
 
-$lang['site_common_main_page_title'] = 'Главная';
-
-
-$lang['site_common_agent'] = 'агент';
-$lang['site_common_agent_upper'] = 'Агент';
-$lang['site_common_for_agents_upper'] = 'Агентам';
-$lang['site_common_agents_page_title'] = 'Заведениям';
-
-$lang['site_common_merchant'] = 'заведение';
-$lang['site_common_merchant_upper'] = 'Заведение';
-$lang['site_common_for_merchants_upper'] = 'Заведениям';
-$lang['site_common_merchants_page_title'] = 'Заведениям';
+$lang['site_common_main_page_title'] = 'Main';
 
 
-$lang['site_common_tourist'] = 'турист';
-$lang['site_common_tourist_upper'] = 'Турист';
+$lang['site_common_agent'] = 'agent';
+$lang['site_common_agent_upper'] = 'Agent';
+$lang['site_common_for_agents_upper'] = 'For Agents';
+$lang['site_common_agents_page_title'] = 'For Businesses';
 
-$lang['site_common_about_page_title'] = 'О нас';
+$lang['site_common_merchant'] = 'business';
+$lang['site_common_merchant_upper'] = 'Business';
+$lang['site_common_for_merchants_upper'] = 'For Businesses';
+$lang['site_common_merchants_page_title'] = 'For Businesses';
 
-$lang['site_common_faq_page_title'] = 'Вопросы и ответы';
 
-$lang['site_common_if_more_questions'] = 'Если у вас остались вопросы, вы найдете ответы на них в разделе';
+$lang['site_common_tourist'] = 'tourist';
+$lang['site_common_tourist_upper'] = 'Tourist';
 
-$lang['site_common_twitter'] = 'Твиттер';
+$lang['site_common_about_page_title'] = 'About us';
+
+$lang['site_common_faq_page_title'] = 'Frequently asked questions';
+
+$lang['site_common_if_more_questions'] = 'If you have any questions, you will find the answers in the section';
+
+$lang['site_common_twitter'] = 'Twitter';
 $lang['site_common_facebook'] = 'Facebook';
