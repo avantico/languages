6b782af3b09427620e68794ef6b7b352a8b97fb2
Ksenia Marinina <ksenia.marinina@glex.com>
2012-12-14 15:23:38 +0400

    footer_lang 100% ready

--- english/site/footer_lang.php
+++ english/site/footer_lang.php
@@ -1,15 +1,15 @@
 <?
 
-$lang['site_footer_top_link']    = "Вверх";
+$lang['site_footer_top_link']    = "Go up";
 $lang['site_footer_rights_reserved'] 	= "© 2012 Glex. All rights reserved.";
 
-$lang['site_footer_subscribe_intro']	= "Читайте главные новости Glex, рассказы о нашей жизни и о новых способах заработка в удобной вам форме";
-$lang['site_footer_subscribe_outro']	= "рассылка не чаще одного раза в месяц";
-$lang['site_footer_subscribe_btn']	= "Подписаться";
+$lang['site_footer_subscribe_intro']	= "Read Glex latest news";
+$lang['site_footer_subscribe_outro']	= "mailing no more than once in a month";
+$lang['site_footer_subscribe_btn']	= "Subscribe";
 
-$lang['site_footer_menu_terms']	= "Условия использования";
+$lang['site_footer_menu_terms']	= "Terms of Use";
 
-$lang['site_footer_address']	= "Glex GmbH, Merckstrasse 9, 91522 Ansbach";
+$lang['site_footer_address']	= "Ozolin S.R.L, Strada della Romagna, 1/4, 61012 Gradara (PU), Italia";
 
 
 
